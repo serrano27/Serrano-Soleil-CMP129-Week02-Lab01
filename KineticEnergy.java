079e85ccc9b3a114198cfa42ad0796e81a18ca38
serrano27 <serrano.soleil@student.ccm.edu>
2026-09-08 14:40:55 -0400

    Complete Week 2 Lab 1

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,27 @@
-public class KineticEnergy {
+import java.util.Scanner;
+public class KineticEnergy 
+{
+    public static void main(String [] args)
+    {
+        Scanner keyboard = new Scanner(System.in); //for user input
+        //vars to help calculate KE
+        double mass, velocity, kE;
+        //ask user for mass
+        System.out.println("Enter the object's mass in kilograms: ");
+        mass = keyboard.nextDouble();
+        //ask user for velocity of object
+        System.out.println("Enter the object's velocity in meters per second: ");
+        velocity = nextDouble();
+        
 
+    }
+    /*
+        kineticEnergy() - accepts object's velocity & mass as parameters, calculates object's
+        kinetic energy, return calculated KE as a double
+    */
+   public static double kineticEnergy(double m, double v) //m is mass, v is velocity
+   {
+        kE = (1/2)*m*Math.pow(v,2);
+        return kE;
+   }
 }
