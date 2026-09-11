c35e8b62222cb6e6fec3c184c3c618c5c40f4ade
serrano27 <serrano.soleil@student.ccm.edu>
2026-09-11 13:20:49 -0400

    Complete Week 2 Lab 1

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,29 @@
-public class CelsiusTable {
-    
+public class CelsiusTable 
+{
+    public static void main(String [] args)
+    {
+        //equation for celsius - C = (5/9) * (F - 32)
+        double Celsius;
+        //display
+        System.out.println("Fahrenheit\tCelsius");
+        System.out.println("------------------------");
+        //for loop for display
+        for (int f = 0; f <= 20; f++)
+        {
+            Celsius = celsius(f);
+            System.out.printf("%d\t\t%.2f",f,Celsius);
+            System.out.println();
+        }
+
+    }
+
+    /*
+        celsius() - accept a Fahrenheit temp as parameter, convert temp to Celsius, return
+        Celsius temp as double
+     */
+    public static double celsius(double f) //f is Fahrenheit
+    {
+        double C = (0.5556) * (f-32); //C is celsius
+        return C;
+    }
 }
